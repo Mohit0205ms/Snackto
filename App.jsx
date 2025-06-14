f78cc8dd03dfdc7abcc63c62759a714ef75234f4
Mohit0205ms <mohitsingh0205ms@gmail.com>
2025-06-14 17:06:40 +0530

    added support for icons

--- App.jsx
+++ App.jsx
@@ -1,41 +1,44 @@
 import React from 'react';
-import { View, Text, StyleSheet, SafeAreaView } from 'react-native';
+import {View, Text, StyleSheet, SafeAreaView} from 'react-native';
 import Onboarding from './src/screens/onboarding/Onboarding';
 import Login from './src/screens/auth/LoginScreen';
-import { colors } from './src/theme/Colors';
+import {colors} from './src/theme/Colors';
 import OTPVerificationScreen from './src/screens/auth/OTPVerificationScreen';
 import AddressListScreen from './src/screens/address/AddressListScreen';
-import { Provider as PaperProvider } from 'react-native-paper';
+import {Provider as PaperProvider} from 'react-native-paper';
 import SelectDeliveryAddressScreen from './src/screens/address/SelectDeliveryAddressScreen';
-// import { SafeAreaView } from 'react-native-safe-area-context';
+import queryClient from './src/libraries/ReactNativeTanStackQueryClientProvider';
+import {QueryClientProvider} from '@tanstack/react-query';
 
-function App(){
+function App() {
   return (
-  <PaperProvider>
-    <SafeAreaView style={styles.safeArea}>
-      <View style={styles.container}>
-        {/* <Onboarding/> */}
-        {/* <Login/> */}
-        {/* <OTPVerificationScreen/> */}
-        {/* <AddressListScreen/> */}
-        <SelectDeliveryAddressScreen/>
-      </View>
-    </SafeAreaView>
-  </PaperProvider>
+    <PaperProvider>
+      <QueryClientProvider client={queryClient}>
+        <SafeAreaView style={styles.safeArea}>
+          <View style={styles.container}>
+            {/* <Onboarding/> */}
+            {/* <Login/> */}
+            {/* <OTPVerificationScreen/> */}
+            <AddressListScreen/>
+            {/* <SelectDeliveryAddressScreen /> */}
+          </View>
+        </SafeAreaView>
+      </QueryClientProvider>
+    </PaperProvider>
   );
 }
 
 const styles = StyleSheet.create({
   safeArea: {
     flex: 1,
-    backgroundColor: colors.white, 
+    backgroundColor: colors.white,
     paddingBottom: 30,
   },
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-  }
+  },
 });
 
 export default App;
--- src/assets/Index.js
+++ src/assets/Index.js
@@ -1,3 +1,18 @@
 export const Images = {
-  onboarding: require('./images/on_boarding.png')
+  onboarding: require('./images/on_boarding.png'),
+}
+
+export const assetsIcon = {
+  add_icon: require('./icons/plus.png'),
+  current_location: require('./icons/current_location.png'),
+  arrow_left: require('./icons/arrow_left.png'),
+  clock: require('./icons/clock.png'),
+  chevron_left: require('./icons/chevron_left.png'),
+  arrow_forward: require('./icons/arrow-forward.png'),
+  share_address: require('./icons/share_address.png'),
+  menu: require('./icons/menu.png'),
+  map_pointer: require('./icons/map_pointer.png'),
+  marker_pin: require('./icons/marker_pin.png'),
+  delete_address: require('./icons/delete.png'),
+  edit_address: require('./icons/edit.png'),
 }
